709732c4fe89d962de424a544ab0885f78211c61
Jesus Berrio <berriojesus122@gmail.com>
2024-03-02 17:25:21 -0500

    Actualizar archivo de configuración de Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 const colors = require("tailwindcss/colors");
-her
+
 module.exports = {
   mode: "jit",
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
