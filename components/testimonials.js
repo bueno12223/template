ea151f2918cf8f3f3d255261402613d045b9dcfd
omar1779 <josemyclans@gmail.com>
2024-03-02 19:32:05 -0500

    add new Mark in Orange

--- components/testimonials.js
+++ components/testimonials.js
@@ -94,7 +94,8 @@ function Mark(props) {
   return (
     <>
       {" "}
-      <mark className="text-indigo-800 bg-indigo-100 rounded-md ring-indigo-100 ring-4 dark:ring-indigo-900 dark:bg-indigo-900 dark:text-indigo-200">
+      <mark className="text-black bg-customYellow rounded-md ring-customYellow ring-4 dark:ring-customYellow  dark:bg-customYellow 
+      dark:text-white">
         {props.children}
       </mark>{" "}
     </>
