0fdb2b8b4a9281e4c890d0ed6a5d720458bfc592
Jesus Berrio <berriojesus122@gmail.com>
2024-03-09 20:24:14 -0500

    Agrega GoogleAdSense a _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "next-themes";
+import { GoogleAdSense } from "nextjs-google-adsense";
 import "../css/tailwind.css";
 
 
@@ -8,6 +9,7 @@ import "slick-carousel/slick/slick-theme.css";
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
+       <GoogleAdSense publisherId="ca-pub-4439391006607431" />
       <Component {...pageProps} />
     </ThemeProvider>
   );
