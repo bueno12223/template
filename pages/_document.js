c1cc6a8724ada3726cf9dd3bd0dc717ab3493d57
Jesus Berrio <berriojesus122@gmail.com>
2024-03-11 21:10:58 -0500

    Actualizar etiqueta de cuenta de Google AdSense en _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -13,12 +13,7 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap"
             rel="stylesheet"
           />
-      <script
-      id="adsbygoogle-init"
-      strategy="afterInteractive"
-      crossOrigin="anonymous"
-      src= "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4439391006607431"
-      />
+          <meta name="google-adsense-account" content="ca-pub-4439391006607431" />
         </Head>
         <body>
           <Main />
