c589918496b0e42f12f7771e6d54c03d64a054a2
Jesus Berrio <berriojesus122@gmail.com>
2024-03-11 20:59:56 -0500

    Agrega script de Google AdSense a _app.js y _document.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import { ThemeProvider } from "next-themes";
 import "../css/tailwind.css";
-import Script from "next/script";
 
 
 import "slick-carousel/slick/slick.css"; 
@@ -9,12 +8,6 @@ import "slick-carousel/slick/slick-theme.css";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-        <Script
-      id="adsbygoogle-init"
-      strategy="afterInteractive"
-      crossOrigin="anonymous"
-      src= "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4439391006607431"
-      />
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
     </ThemeProvider>
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import Link from "next/link";
+import Script from "next/script";
 
 class MyDocument extends Document {
   render() {
@@ -12,6 +13,12 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap"
             rel="stylesheet"
           />
+      <Script
+      id="adsbygoogle-init"
+      strategy="afterInteractive"
+      crossOrigin="anonymous"
+      src= "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4439391006607431"
+      />
         </Head>
         <body>
           <Main />
