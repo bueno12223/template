369425725cc2467d0d0db16ad3b56ee295b6c726
Omar Sosa <omardeveloper@Tests-MacBook-Pro.local>
2024-09-14 18:44:55 -0500

    por fin funciona

--- components/floatingbutton.js
+++ components/floatingbutton.js
@@ -4,7 +4,7 @@ const FloatingButton = ({ onClick, children }) => {
   return (
     <button
       onClick={onClick}
-      className="fixed bottom-5 right-5 bg-blue-500 text-white rounded-full w-16 h-16 flex items-center justify-center shadow-lg hover:bg-blue-600 focus:outline-none"
+      className="fixed bottom-5 right-5 bg-blue-500 text-white rounded-full w-16 h-16 flex items-center justify-center shadow-lg hover:bg-blue-600 focus:outline-none z-50"
     >
       {children}
     </button>
